35936de479443085f4b1e63cbcd63f1893e817e1
Piotr Zawodnik <109288335+Tangoblu9@users.noreply.github.com>
2022-07-25 13:08:20 +0200

    Update and rename dvd_rental.sql to DVD_payments.sql

--- DVD_payments.sql
+++ DVD_payments.sql
@@ -1,3 +1,5 @@
+/*This is to extract total amount of dvd rental payments from selected staff_id, and period. Date time converted to specific "Month year" format      */
+
 SELECT DISTINCT(TO_CHAR(payment_date, 'MONTH YYYY')) AS TIME_PERIOD, staff_id, sum(amount)
 FROM payment
 WHERE payment_date BETWEEN '2007-02-01' AND '2007-05-30'
