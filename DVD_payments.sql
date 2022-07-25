fc7814ce3edd1a5235e7ea2f81144022a9feac88
Piotr Zawodnik <109288335+Tangoblu9@users.noreply.github.com>
2022-07-25 13:20:13 +0200

    Update DVD_payments.sql

--- DVD_payments.sql
+++ DVD_payments.sql
@@ -1,5 +1,8 @@
 /*This is to extract total amount of dvd rental payments from selected staff_id, and period. Date time converted to specific "Month year" format      */
 
+--version: PostgreSQL 14.0
+--input data: dvd_rental database
+
 SELECT DISTINCT(TO_CHAR(payment_date, 'MONTH YYYY')) AS TIME_PERIOD, staff_id, sum(amount)
 FROM payment
 WHERE payment_date BETWEEN '2007-02-01' AND '2007-05-30'
